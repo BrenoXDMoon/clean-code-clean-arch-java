4a3efe40b7de09e2a11fc1589e94b74ef0e8936d
Breno Gabriel <brenogabriel19@gmail.com>
2023-11-29 21:47:56 -0300

    Initial Commit

---
+++ src/main/java/br/com/brenoxdmoon/cleancodecleanarchjava/CleanCodeCleanArchJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.cleancodecleanarchjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeCleanArchJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeCleanArchJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brenoxdmoon/cleancodecleanarchjava/CleanCodeCleanArchJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.cleancodecleanarchjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeCleanArchJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
